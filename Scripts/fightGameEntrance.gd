aeff0d94569a17fd88f5711dae12b7f33ff5156e
Parker <psmi612@wgu.edu>
2025-09-26 17:15:57 -0500

    another test comment commit

--- Scripts/fightGameEntrance.gd
+++ Scripts/fightGameEntrance.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 #i made this comment for GIT
+#no I made this comment for git
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if interact and canFight:
